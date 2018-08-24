47702d4f8f008e2b1bd287bdaacde1df520d62f5
Senthil Kumar Ganesan <skg.net.dev@gmail.com>
2018-08-24 14:31:07 -0700

    Addressed review comments
    Signed-off-by: Senthil Kumar Ganesan <skg.net.dev@gmail.com>

--- inst-hooks/03-disable-ztd-file.pre-upgrade.sh
+++ inst-hooks/03-disable-ztd-file.pre-upgrade.sh
@@ -3,4 +3,6 @@
 
 # Deleting the postinst hook ensures that even a failure in a later hook
 # would not leave the ZTD file hanging around on the standby filesystem.
+# This is will remove the install hook
+# when we upgrade the bin in standby partition alone.
 rm -f /alt/root/hooks/95-enable-ztd-mode.postinst.sh
